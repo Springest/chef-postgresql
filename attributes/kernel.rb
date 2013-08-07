d55e24be3e429140014095fa706c0f86cf8f1f5d
Wouter de Vos <wouter@springest.com>
2013-08-07 12:13:40 +0200

    Set shared memory size in sysctl config.

--- attributes/kernel.rb
+++ attributes/kernel.rb
@@ -1,4 +1,4 @@
 include_attribute 'postgres::default'
 
-default['postgres']['kernel']['shmmax'] = nil
-default['postgres']['kernel']['page_size'] = nil
+default['postgres']['kernel']['shmmax'] = 33554432
+default['postgres']['kernel']['page_size'] = 4096
--- recipes/kernel.rb
+++ recipes/kernel.rb
@@ -1,14 +1,4 @@
-if node['postgres']['kernel']['shmmax']
-  shmmax = node['postgres']['kernel']['shmmax']
-  shmall = node['postgres']['kernel']['shmmax'] / node['postgres']['kernel']['page_size']
+shmmax = node['postgres']['kernel']['shmmax']
+node.set['postgres']['kernel']['shmall'] = node['postgres']['kernel']['shmmax'] / node['postgres']['kernel']['page_size']
 
-  execute "kernel-resources-shmmax" do
-    command "sysctl -w kernel.shmmax=#{shmmax}; sysctl -w kernel.shmall=#{shmmax};"
-    not_if { `cat /proc/sys/kernel/shmmax`.chomp.to_i == shmmax }
-  end
-
-  execute "kernel-resources-shmall" do
-    command "sysctl -w kernel.shmall=#{shmall};"
-    not_if { `cat /proc/sys/kernel/shmall`.chomp.to_i == shmall }
-  end
-end
+template "/etc/sysctl.d/30-postgresql-shm.conf" { mode '0644' }
