2823a6205b2a806e29ed01a9f956235461d28d36
Wouter de Vos <wouter@springest.com>
2013-08-07 12:17:37 +0200

    Fix cookbook.

--- recipes/kernel.rb
+++ recipes/kernel.rb
@@ -1,4 +1,6 @@
 shmmax = node['postgres']['kernel']['shmmax']
 node.set['postgres']['kernel']['shmall'] = node['postgres']['kernel']['shmmax'] / node['postgres']['kernel']['page_size']
 
-template "/etc/sysctl.d/30-postgresql-shm.conf" { mode '0644' }
+template "/etc/sysctl.d/30-postgresql-shm.conf" do
+  mode '0644'
+end
