4302e06cf71ef74874bb588af21ce9394252eb53
Wouter de Vos <wouter@springest.com>
2013-08-07 12:20:02 +0200

    Fixed source for template.

--- recipes/kernel.rb
+++ recipes/kernel.rb
@@ -2,5 +2,6 @@ shmmax = node['postgres']['kernel']['shmmax']
 node.set['postgres']['kernel']['shmall'] = node['postgres']['kernel']['shmmax'] / node['postgres']['kernel']['page_size']
 
 template "/etc/sysctl.d/30-postgresql-shm.conf" do
+  source "postgresql-shm.conf.erb"
   mode '0644'
 end
