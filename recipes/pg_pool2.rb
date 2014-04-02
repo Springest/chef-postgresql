a9921a9c777fe00f6f32e5afe1cc84978a39badb
Milo de Vries <milo@xdh.nl>
2014-04-02 19:25:43 +0200

    Extension of pgpool2 recipe

--- recipes/pg_pool2.rb
+++ recipes/pg_pool2.rb
@@ -1,5 +1,16 @@
 package "pgpool2"
 
-if node['postgres']['pgpool2'] && node['postgres']['pgpool2']['users']
-  template "/etc/pgpool2/pcp.conf"
+include_recipe "postgres::kernel"
+
+template "/etc/pgpool2/pcp.conf" do
+	source "pcp.conf.erb"
+	action :create
+	owner "root"
+	only_if { node['postgres']['pgpool2'] && node['postgres']['pgpool2']['users'] }
+end
+
+template "/etc/pgpool2/pgpool.conf" do
+  source "pgpool.conf.erb"
+  owner "root"
+  action :create
 end
--- recipes/pg_pool2_server_support.rb
+++ recipes/pg_pool2_server_support.rb
@@ -1,4 +1,13 @@
 package "postgresql-#{node["postgres"]["version"]}-pgpool2"
 
+template "/var/chef_file_cache/pgpool-regclass.sql" do
+  source "pgpool-regclass.sql.erb"
+  owner "root"
+  action :create
+end
+
 template "/var/chef_file_cache/pgpool-recovery.sql"
-template "/var/chef_file_cache/pgpool-regclass.sql"
+  source "pgpool-recovery.sql.erb"
+  owner "root"
+  action :create
+end
