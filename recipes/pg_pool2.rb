424787772da2009dddd58e03ed79ddea3702efbc
Wouter de Vos <wouter@springest.com>
2014-04-03 13:37:49 +0200

    :tropical_fish:

--- recipes/pg_pool2.rb
+++ recipes/pg_pool2.rb
@@ -3,10 +3,10 @@ package "pgpool2"
 include_recipe "postgres::kernel"
 
 template "/etc/pgpool2/pcp.conf" do
-	source "pcp.conf.erb"
-	action :create
-	owner "root"
-	only_if { node['postgres']['pgpool2'] && node['postgres']['pgpool2']['users'] }
+  source "pcp.conf.erb"
+  action :create
+  owner "root"
+  only_if { node['postgres']['pgpool2'] && node['postgres']['pgpool2']['users'] }
 end
 
 template "/etc/pgpool2/pgpool.conf" do
