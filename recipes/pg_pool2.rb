6874bac23626734a3d923adb7807837aed153e13
Wouter de Vos <wouter@springest.com>
2014-04-03 17:16:06 +0200

    Create pool_hba.conf.

--- recipes/pg_pool2.rb
+++ recipes/pg_pool2.rb
@@ -22,6 +22,14 @@ template "/etc/pgpool2/pgpool.conf" do
   notifies :reload, 'service[pgpool2]'
 end
 
+template "/etc/pgpool2/pool_hba.conf" do
+  source "pg_hba.conf.erb"
+  owner  "postgres"
+  group  "postgres"
+  mode   "0640"
+  notifies :reload, "service[postgresql]"
+end
+
 service "pgpool2" do
   supports :restart => true, :status => true, :reload => true
   action [:enable, :start]
