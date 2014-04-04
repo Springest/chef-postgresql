5d09fc3041c7c040acee698336956db58ba0d734
Wouter de Vos <wouter@springest.com>
2014-04-04 08:38:40 +0200

    Create pgpool functions and insert lock table.

--- recipes/pg_pool2_server_support.rb
+++ recipes/pg_pool2_server_support.rb
@@ -4,10 +4,33 @@ template "/var/chef_file_cache/pgpool-regclass.sql" do
   source "pgpool-regclass.sql.erb"
   owner "root"
   action :create
+  notifies :run, 'execute[create-pgpool-regclass]', :immediately
+end
+
+execute "create-pgpool-regclass" do
+  command "psql -f /var/chef_file_cache/pgpool-regclass.sql template1"
+  user "postgres"
 end
 
 template "/var/chef_file_cache/pgpool-recovery.sql" do
   source "pgpool-recovery.sql.erb"
   owner "root"
   action :create
+  notifies :run, 'execute[create-pgpool-recovery]', :immediately
+end
+
+execute "create-pgpool-recovery" do
+  command "psql -f /var/chef_file_cache/pgpool-recovery.sql template1"
+  user "postgres"
+end
+
+cookbook_file "/var/chef_file_cache/insert_lock.sql" do
+  owner "root"
+  action :create
+  notifies :run, 'execute[create-pgpool-insert-lock]', :immediately
+end
+
+execute "create-pgpool-insert-lock" do
+  command "psql -f /var/chef_file_cache/create-pgpool-insert-lock.sql template1"
+  user "postgres"
 end
