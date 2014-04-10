cadad62b892621b31c514707b9c4e9131f41bbce
Wouter de Vos <wouter@springest.com>
2014-04-10 13:58:30 +0200

    Online recovery for pgpool cluster servers.

---
+++ recipes/replication_cluster.rb
@@ -0,0 +1,16 @@
+template "#{node["postgres"]["data_directory"]}/pgpool_recovery" do
+  mode "0755"
+  owner "postgres"
+end
+
+template "#{node["postgres"]["data_directory"]}/pgpool_recovery2" do
+  mode "0755"
+  owner "postgres"
+end
+
+directory node['postgres']['archive_directory'] do
+  action :create
+  owner 'postgres'
+  group 'postgres'
+  recursive true
+end
