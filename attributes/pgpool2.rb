7393167ad56ed264677fea9cd68ac037eefa3666
Wouter de Vos <wouter@springest.com>
2014-04-11 12:16:45 +0200

    connection_life_time configurable.

--- attributes/pgpool2.rb
+++ attributes/pgpool2.rb
@@ -1,4 +1,5 @@
 # Maximum socket connections
-default['postgres']['pgpool2']['somaxconn']            = 128
-default['postgres']['pgpool2']['num_init_children']    = 50
-default['postgres']['pgpool2']['max_pool']             = 4
+default['postgres']['pgpool2']['somaxconn']               = 128
+default['postgres']['pgpool2']['num_init_children']       = 50
+default['postgres']['pgpool2']['max_pool']                = 4
+default['postgres']['pgpool2']['connection_life_time']    = 0
