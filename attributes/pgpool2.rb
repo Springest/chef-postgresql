516478bcc677c46650fb5395029f13d77b34b90b
Wouter de Vos <wouter@springest.com>
2014-12-18 15:10:18 +0100

    Make Pgpool2 client_idle_limit configurable.

--- attributes/pgpool2.rb
+++ attributes/pgpool2.rb
@@ -3,3 +3,4 @@ default['postgres']['pgpool2']['somaxconn']               = 128
 default['postgres']['pgpool2']['num_init_children']       = 50
 default['postgres']['pgpool2']['max_pool']                = 4
 default['postgres']['pgpool2']['connection_life_time']    = 0
+default['postgres']['pgpool2']['client_idle_limit']       = 0
